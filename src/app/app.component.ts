05ce515cb40ba343e147f4a8f0862da21d34f373
nlilaramani <nlilaramani@hotmail.com>
2025-01-21 21:57:14 -0600

    initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from "./user-list/user-list.component";
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { Route, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserListComponent ],
   templateUrl: './app.component.html',
+  
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-first';
+  constructor(){
+    //RouterModule.forRoot(routes);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { UserListComponent } from './user-list/user-list.component';
+export const routes:Routes=[
+  {path:'',component:HomeComponent},
+  {path:'users',component:UserListComponent},
+  {path:'aboutus',component:AboutusComponent}
+];
